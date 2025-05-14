26853c0e53e06ed16a6d57530974937a5be8d062
DafferNx <filbertdeven20@gmail.com>
2025-05-14 19:31:53 +0700

    Class Game V0.1 add method atribut

--- src/main/java/app/steam/Game.java
+++ src/main/java/app/steam/Game.java
@@ -1,5 +1,28 @@
 package app.steam;
 
 public abstract class Game implements Playable{
+    private String gameId, title;
+    private double price;
     
+    public Game(String gameId, String title, double price) {
+        this.gameId = gameId;
+        this.title = title;
+        this.price = price;
+    }
+
+    public void download(){
+
+    }
+
+    public void play(){
+
+    }
+
+    public void exit(){
+
+    }
+
+    public String getDetail(){
+        return "";
+    }
 }
