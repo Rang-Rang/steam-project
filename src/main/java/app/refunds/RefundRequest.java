56495eb5b3ef718b9def38e20293b112d71e5abd
Zikry <ilovegreen875@gmail.com>
2025-05-22 15:44:43 +0700

    add refund

--- src/main/java/app/refunds/RefundRequest.java
+++ src/main/java/app/refunds/RefundRequest.java
@@ -1,5 +1,28 @@
 package app.refunds;
 
+import app.steam.Game;
+
 public class RefundRequest {
+    private String requestId;
+    private Game game;
+    private String userId;
+    private String status;
+    private String reason;
     
+    public RefundRequest(String requestId, Game game, String userId, String status, String reason) {
+        this.requestId = requestId;
+        this.game = game;
+        this.userId = userId;
+        this.status = status;
+        this.reason = reason;
+    }
+
+        public void submit() {
+            this.status = "Terkirim";
+            System.out.println("Permintaan pengembalian dana sudah terkirim oleh user: " + userId);
+        }
+
+        public String getStatus() {
+            return status;
+        }
 }
--- src/main/java/app/refunds/RefundRequester.java
+++ src/main/java/app/refunds/RefundRequester.java
@@ -1,5 +1,7 @@
 package app.refunds;
 
+import app.steam.Game;
+
 public interface RefundRequester {
-    
+    RefundRequest requestRefund(Game game);
 }
